ef31e73e67e2112242c58b4aa9ffdda42ca7d0e2
rohanranjan501 <rohanranjan5555@gmail.com>
2026-07-12 20:10:23 +0530

    Initial

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnapLedger
+//
+//  Created by Rohan Ranjan on 12/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnapLedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapLedgerApp.swift
+//  SnapLedger
+//
+//  Created by Rohan Ranjan on 12/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SnapLedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
